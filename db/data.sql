e7b6ba80ce6c49cfcc28bfb25718f1d6be4c9818
ReyFey <vic.betsch@gmail.com>
2022-06-05 19:34:40 +0200

    :seedling: Added database seeder

---
+++ db/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO rpg.player (id, name, age) VALUES (1, 'Victor', 20);
+INSERT INTO rpg.player (id, name, age) VALUES (2, 'Louis', 10);
+
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (1, 'mage', 50, 30, 60, 120);
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (2, 'assassin', 60, 80, 100, 10);
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (3, 'guerrier', 70, 50, 30, 20);
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (4, 'voleur', 40, 70, 100, 10);
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (5, 'tank', 150, 30, 10, 0);
+INSERT INTO rpg.role (id, label, pv, pa, pm, mana) VALUES (6, 'healer', 30, 10, 60, 100);
---
+++ db/seeder.sql
@@ -0,0 +1,45 @@
+drop database if exists rpg;
+create database if not exists rpg;
+use rpg;
+
+create table if not exists player
+(
+    id   int auto_increment
+        primary key,
+    name varchar(64) not null,
+    age  int         null,
+    constraint player_id_uindex
+        unique (id)
+);
+
+create table if not exists role
+(
+    id    int auto_increment
+        primary key,
+    label varchar(64) not null,
+    pv    int         null,
+    pa    int         null,
+    pm    int         null,
+    mana  int         null,
+    constraint role_id_uindex
+        unique (id)
+);
+
+create table if not exists personnage
+(
+    id        int auto_increment
+        primary key,
+    player_id int         not null,
+    role_id   int         not null,
+    name      varchar(64) not null,
+    sexe      char        null,
+    age       int         null,
+    constraint personnage_id_uindex
+        unique (id),
+    constraint personnage_player_id_fk
+        foreign key (player_id) references player (id),
+    constraint personnage_role_id_fk
+        foreign key (role_id) references role (id)
+);
+
+
