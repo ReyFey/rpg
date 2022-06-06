34981b12340104029597dc434751ff859058730c
ReyFey <vic.betsch@gmail.com>
2022-06-06 14:49:04 +0200

    :zap: Replace `rpg` db by db property name

--- Database.py
+++ Database.py
@@ -1,17 +1,6 @@
 import mysql.connector
 
 
-def sql_insert(table, properties, nb_values):
-    sql = f"INSERT INTO rpg.{table} ({properties[0]}"
-    for index in range(1, len(properties)):
-        sql += f", {properties[index]}"
-    sql += f") VALUES (%s"
-    for index in range(1, nb_values):
-        sql += ", %s"
-    sql += ");"
-    return sql
-
-
 class Database:
     def __init__(self, host, port, user, password, name):
         self.host = host
@@ -32,26 +21,40 @@ class Database:
         self.cursor.execute(request)
         return self.cursor.fetchall()
 
+    def sql_insert(self, table, properties, nb_values):
+        sql = f"INSERT INTO {self.name}.{table} ({properties[0]}"
+        for index in range(1, len(properties)):
+            sql += f", {properties[index]}"
+        sql += f") VALUES (%s"
+        for index in range(1, nb_values):
+            sql += ", %s"
+        sql += ");"
+        return sql
+
     def insert_one(self, table, properties, values):
-        self.cursor.execute(sql_insert(table, properties, len(values)), values)
+        self.cursor.execute(self.sql_insert(table, properties, len(values)), values)
         self.connector.commit()
 
     def insert_many(self, table, properties, values):
-        self.cursor.executemany(sql_insert(table, properties, len(values)), values)
+        self.cursor.executemany(self.sql_insert(table, properties, len(values)), values)
         self.connector.commit()
 
     def update(self, table, properties, conditions, values):
-        sql = f"UPDATE rpg.{table} SET {properties[0]} = %s"
+        sql = f"UPDATE {self.name}.{table} SET {properties[0]} = %s"
         for index in range(1, len(properties)):
             sql += f", {properties[1]} = %s"
         sql += f" WHERE {conditions};"
         self.cursor.execute(sql, values)
         self.connector.commit()
 
-    def delete_all(self, table):
-        self.cursor.execute(f"DELETE FROM rpg.{table} WHERE 1")
+    def clear_table(self, table):
+        self.cursor.execute(f"DELETE FROM {self.name}.{table} WHERE 1")
         self.connector.commit()
 
+    def clear_all(self):
+        # TODO : Clear all tables by select
+        pass
+
     def delete_by(self, table, conditions, values):
-        self.cursor.execute(f"DELETE FROM rpg.{table} WHERE {conditions}", values)
+        self.cursor.execute(f"DELETE FROM {self.name}.{table} WHERE {conditions}", values)
         self.connector.commit()
